1f937d60a7c0a5f1d221a616d9975e5a42caf353
adarachel <adarachelofficial@gmail.com>
2023-05-05 12:28:11 +0100

    fix linter errors

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    testEnvironment: 'jsdom',
-    setupFilesAfterEnv: ['./test-setup.js'],
-    transform: {
-      '^.+\\.jsx?$': 'babel-jest',
-      "^.+\\.css$": "jest-transform-stub",
-    },
-  };
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['./test-setup.js'],
+  transform: {
+    '^.+\\.jsx?$': 'babel-jest',
+    '^.+\\.css$': 'jest-transform-stub',
+  },
+};
